fb194ba28a8905c7415791e4b80556f8ec37e4ba
n3rdz <n3rdz@users.noreply.github.com>
2015-10-03 18:45:21 +0200

    update ndr url

--- addon.py
+++ addon.py
@@ -8,13 +8,15 @@ li = xbmcgui.ListItem(label='DASDING Visual Radio Stream')
 url = 'http://swr3visual-lh.akamaihd.net/i/swr3visual_live@6410/master.m3u8'
 li = xbmcgui.ListItem(label='SWR3 Visual Radio Stream')
 url = 'http://swr_event01_uni-lh.akamaihd.net/i/swr_event01@112805/master.m3u8'
-li = xbmcgui.ListItem(label='Event Stream 01')
+li = xbmcgui.ListItem(label='SWR Event Stream 01')
 url = 'http://swr_event02_uni-lh.akamaihd.net/i/swr_event02@112806/master.m3u8'
-li = xbmcgui.ListItem(label='Event Stream 02')
+li = xbmcgui.ListItem(label='SWR Event Stream 02')
 url = 'http://swr_event03_uni-lh.akamaihd.net/i/swr_event03@198168/master.m3u8'
-li = xbmcgui.ListItem(label='Event Stream 03')
+li = xbmcgui.ListItem(label='SWR Event Stream 03')
 url = 'http://swr_event04_uni-lh.akamaihd.net/i/swr_event04@198169/master.m3u8'
-li = xbmcgui.ListItem(label='Event Stream 04')
+li = xbmcgui.ListItem(label='SWR Event Stream 04')
+url = 'http://ndr_events-lh.akamaihd.net/i/ndrevent_1@119220/master.m3u8'
+li = xbmcgui.ListItem(label='NDR Event Stream 01')
 li.setIconImage('DefaultVideo.png')
 li.setThumbnailImage('icon.png')
 li.setProperty('fanart_image', 'visualradio.png')
