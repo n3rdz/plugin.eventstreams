1ffc0618a5a46371bd4ffe6403a6ca355c3fda37
n3rdz <n3rdz@users.noreply.github.com>
2016-02-14 16:56:03 +0100

    updated
    funktioniert jetzt endlich

--- addon.py
+++ addon.py
@@ -1,24 +1,41 @@
 import sys
 import xbmcgui
 import xbmcplugin
+
 addon_handle = int(sys.argv[1])
-xbmcplugin.setContent(addon_handle, 'audio')
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
-li = xbmcgui.ListItem(label='DASDING Visual Radio Stream')
-url = 'http://swr3visual-lh.akamaihd.net/i/swr3visual_live@6410/master.m3u8'
-li = xbmcgui.ListItem(label='SWR3 Visual Radio Stream')
-url = 'http://swr_event01_uni-lh.akamaihd.net/i/swr_event01@112805/master.m3u8'
-li = xbmcgui.ListItem(label='SWR Event Stream 01')
-url = 'http://swr_event02_uni-lh.akamaihd.net/i/swr_event02@112806/master.m3u8'
-li = xbmcgui.ListItem(label='SWR Event Stream 02')
-url = 'http://swr_event03_uni-lh.akamaihd.net/i/swr_event03@198168/master.m3u8'
-li = xbmcgui.ListItem(label='SWR Event Stream 03')
-url = 'http://swr_event04_uni-lh.akamaihd.net/i/swr_event04@198169/master.m3u8'
-li = xbmcgui.ListItem(label='SWR Event Stream 04')
-url = 'http://ndr_events-lh.akamaihd.net/i/ndrevent_1@119220/master.m3u8'
-li = xbmcgui.ListItem(label='NDR Event Stream 01')
-li.setIconImage('DefaultVideo.png')
-li.setThumbnailImage('icon.png')
-li.setProperty('fanart_image', 'visualradio.png')
+li = xbmcgui.ListItem('DASDING Visual Radio')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://swr3visual-lh.akamaihd.net/i/swr3visual_live@6410/master.m3u8'
+li = xbmcgui.ListItem('SWR3 Visual Radio')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://swr_event01_uni-lh.akamaihd.net/i/swr_event01@112805/master.m3u8'
+li = xbmcgui.ListItem('SWR Event1')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://swr_event02_uni-lh.akamaihd.net/i/swr_event02@112806/master.m3u8'
+li = xbmcgui.ListItem('SWR Event2')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://swr_event03_uni-lh.akamaihd.net/i/swr_event03@198168/master.m3u8'
+li = xbmcgui.ListItem('SWR Event3')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://swr_event04_uni-lh.akamaihd.net/i/swr_event04@198169/master.m3u8'
+li = xbmcgui.ListItem('SWR Event4')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://ndr_events-lh.akamaihd.net/i/ndrevent_1@119220/master.m3u8'
+li = xbmcgui.ListItem('NDR Event1')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://brevent1hds-lh.akamaihd.net/i/br_event02isma@111248/master.m3u8'
+li = xbmcgui.ListItem('BR Event2')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
 xbmcplugin.endOfDirectory(addon_handle)
